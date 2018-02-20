1d7098fea1162d62d640d8cdd727d337c1135df8
godofnoobs <n_ska@bk.ru>
2018-02-20 22:29:58 +0300

    sorting numbers by default realized

--- src/index.js
+++ src/index.js
@@ -1,31 +1,65 @@
+'use strict'
+
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        this.storage = {};
+        this._compareFunction = (left, right) => right - left;
+        this.length = 0;
+    }
 
-  add(element) {
-    // your implementation
-  }
+    add(element) {
+        this.storage[this.length] = element;
+        this.length += 1;
+    }
 
-  at(index) {
-    // your implementation
-  }
+    at(index) {
+        return this.storage[index];
+    }
 
-  get length() {
-    // your implementation
-  }
+    //length() {
+    //  return this._currentLength;
+    //}
 
-  toArray() {
-    // your implementation
-  }
+    toArray() {
+        var arr = [];
+        for (var i = 0; i < this.length; i++)
+            arr[i] = this.storage[i];
+        return arr;
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    compare(a, b) {
+        if (this._compareFunction(a, b) > 0)
+            return true;
+        return false;
+    }
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+    sort(indices) {
+        var n = indices.length;
+        for (var i = 0; i < n - 1; i++) {
+            for (var j = 0; j < n - 1 - i; j++) {
+                if (indices[j + 1] < indices[j]) {
+                    var t = indices[j + 1];
+                    indices[j + 1] = indices[j]
+                    indices[j] = t;
+                }
+            }
+        }
+        for (var i = 0; i < n - 1; i++) {
+            for (var j = 0; j < n - 1 - i; j++) {
+                var a = this.storage[indices[j + 1]];
+                var b = this.storage[indices[j]];
+                if (this.compare(a, b)) {
+                    this.storage[indices[j + 1]] = b;
+                    this.storage[indices[j]] = a;
+                }
+            }
+        }
+
+    }
+
+    setComparator(compareFunction) {
+        this._compareFunction = eval(compareFunction);
+    }
 }
 
 module.exports = Sorter;
